c2ea61b7f133e95fca979a42390cc053b3c50f32
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2024-06-18 14:26:18 -0300

    Video Link

--- src/app/Components/Comparação.tsx
+++ src/app/Components/Comparação.tsx
@@ -6,7 +6,10 @@ import ComparisonPoster from "@/Assets/images/Posters/Comparison.png";
 
 const Comparação = () => {
   return (
-    <div className="w-full lg:flex flex-col items-center lg:h-[125vh] gap-6 px-4 py-8 overflow-hidden" id="Comparação">
+    <div
+      className="w-full lg:flex flex-col items-center lg:h-[125vh] gap-6 px-4 py-8 overflow-hidden"
+      id="Comparação"
+    >
       <div className="w-full lg:w-[90%]">
         <span className="font-black Inter text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl tracking-[0.1em] text-left">
           COMPARAÇÃO
@@ -14,7 +17,7 @@ const Comparação = () => {
       </div>
       <div className="w-full lg:w-[90%] h-[30vh] lg:h-[90%] relative">
         <video
-          src="https://ik.imagekit.io/z3fr9lhps/Life%20is%20Strange%20Page/ComparisonVideo.mp4?updatedAt=1718635555289"
+          src="https://kc1t.github.io/LifeIsStrangePage/assets/videos/ComparisonVideo.mp4"
           className="absolute inset-0 object-cover w-full h-full z-[0] select-none cursor-pointer"
           controls
           poster="https://img.youtube.com/vi/CjNl-enDTUM/maxresdefault.jpg"
